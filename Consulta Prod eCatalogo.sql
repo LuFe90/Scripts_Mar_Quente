12de95ba675adbfa681cd73d533019b44a7d769a
LuFe90 <55301450+LuFe90@users.noreply.github.com>
2025-04-25 17:30:54 -0300

    Create Consulta Prod eCatalogo.sql

---
+++ Consulta Prod eCatalogo.sql
@@ -0,0 +1,79 @@
+                        SELECT B.DESC_PRODUTO,
+                               B.PRODUTO,
+                               B.GRIFFE,
+                               B.GRUPO_PRODUTO,
+                               D.COD_FILIAL,
+                               B.TIPO_PRODUTO,
+                               B.DESC_PRODUTO,
+                               ''       AS COMPOSITION,
+                               ''       AS TECHNICAL_INFO,
+                               B.CLASSIF_FISCAL,
+                               E.DESC_COLECAO,
+                               F.PRECO1,
+	                           G.CODIGO_BARRA,
+	                           A.DISPONIVEL,
+	                           H.QTDE
+                        FROM   MQ_ESTOQUE_DISPONIVEL_PEDIDO_NEW AS A
+                               INNER JOIN PRODUTOS AS B
+                                       ON A.PRODUTO = B.PRODUTO
+                               INNER JOIN PRODUTOS_PACKS_PERMITIDOS AS C
+                                       ON A.PRODUTO = C.PRODUTO
+                               INNER JOIN FILIAIS AS D
+                                       ON A.FILIAL = D.FILIAL
+                                          AND D.ECATALOGOSLOJA = 1
+                               INNER JOIN COLECOES AS E
+                                       ON B.COLECAO = E.COLECAO
+                               INNER JOIN PRODUTOS_PRECOS AS F
+                                       ON B.PRODUTO = F.PRODUTO
+                                          AND F.CODIGO_TAB_PRECO = '01'
+                               INNER JOIN PRODUTOS_BARRA AS G
+                                       ON B.PRODUTO = G.PRODUTO
+                                          AND G.CODIGO_BARRA LIKE '7%'
+	                           INNER JOIN PRODUTOS_PACKS_PERMITIDOS AS H
+			                           ON B.PRODUTO = H.PRODUTO
+                        WHERE	B.PRODUTO NOT IN (SELECT PRODUTO FROM MQ_INTEGRACAO_eCATALOGOS_LOJAS_PRODUTOS AS X 
+                                                  WHERE X.PRODUTO = B.PRODUTO )
+								AND	DISPONIVEL >= C.QTDE
+                            
+						  and b.produto in ('10.10.0112')
+
+
+						  		SELECT * FROM MQ_ESTOQUE_DISPONIVEL_PEDIDO_NEW WHERE PRODUTO = '10.10.0112'
+-- ESTOQUE ENVIADO
+
+                        SELECT B.DESC_PRODUTO,
+                               B.PRODUTO,
+                               B.GRIFFE,
+                               B.GRUPO_PRODUTO,
+                               D.COD_FILIAL,
+                               B.TIPO_PRODUTO,
+                               B.DESC_PRODUTO,
+                               ''       AS COMPOSITION,
+                               ''       AS TECHNICAL_INFO,
+                               B.CLASSIF_FISCAL,
+                               E.DESC_COLECAO,
+                               F.PRECO1,
+	                           G.CODIGO_BARRA,
+	                           A.DISPONIVEL,
+	                           H.QTDE,
+							   D.FILIAL
+                        FROM   MQ_ESTOQUE_DISPONIVEL_PEDIDO_NEW AS A
+                               INNER JOIN PRODUTOS AS B
+                                       ON A.PRODUTO = B.PRODUTO
+                               INNER JOIN PRODUTOS_PACKS_PERMITIDOS AS C
+                                       ON A.PRODUTO = C.PRODUTO
+                               INNER JOIN FILIAIS AS D
+                                       ON A.FILIAL = D.FILIAL
+                                          AND D.ECATALOGOSLOJA = 1
+                               INNER JOIN COLECOES AS E
+                                       ON B.COLECAO = E.COLECAO
+                               INNER JOIN PRODUTOS_PRECOS AS F
+                                       ON B.PRODUTO = F.PRODUTO
+                                          AND F.CODIGO_TAB_PRECO = '01'
+                               INNER JOIN PRODUTOS_BARRA AS G
+                                       ON B.PRODUTO = G.PRODUTO
+                                          AND G.CODIGO_BARRA LIKE '7%'
+	                           INNER JOIN PRODUTOS_PACKS_PERMITIDOS AS H
+			                           ON B.PRODUTO = H.PRODUTO
+                        WHERE	B.eCatalogosLoja = '1'
+								AND	DISPONIVEL >= C.QTDE
