6d8d9853303b9e9b13b2e1e6498e0a684786732d
LuFe90 <55301450+LuFe90@users.noreply.github.com>
2025-09-19 08:49:38 -0300

    Create HOMOL_TABELA_CLIENTES_CONVERTR.sql
    Tabela utilizada para fazer a integração dos clientes da convertr com  Linx - em testes

---
+++ HOMOL_TABELA_CLIENTES_CONVERTR.sql
@@ -0,0 +1,6 @@
+----------------------------------------------------------------
+--Ambiente Teste - Tabela de registro dos clientes da Convertr--
+----------------Realizar de <> para com Linx--------------------
+----------------------------------------------------------------
+
+SELECT * FROM MQ_CLIENTE_TEMPORARIO
